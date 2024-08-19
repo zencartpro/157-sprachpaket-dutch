d9996163d3595eb25e364e074d6b579d57695df1
Jörg Schönberger <office@webchills.at>
2024-08-19 12:38:55 +0200

    Version 1.3.1

---
+++ NEUE DATEIEN/includes/languages/dutch/instant_search_result.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package  Instant Search Plugin for Zen Cart German
+ * @author   marco-pm
+ * @version  4.0.3
+ * @see      https://github.com/marco-pm/zencart_instantsearch
+ * @license  GNU Public License V2.0
+ * modified for Zen Cart German
+ * 2024-08-19 webchills
+ */
+
+define('NAVBAR_TITLE', 'Zoekresultaten');
+define('HEADING_TITLE', 'Zoekresultaten');
+define('TEXT_NO_PRODUCTS_FOUND', 'Geen artikelen gevonden');
+define('TEXT_LOADING_RESULTS', 'Laad resultaten...');
+define('TEXT_SHOW', 'Filter resultaten op:');
